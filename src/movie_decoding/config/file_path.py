7e542d385f4b99e2f3315c76b54201df28a5329f
Xin <michaelniki1988@gmail.com>
2024-10-02 11:29:10 -0700

    [refactor] put all path in one file

--- src/movie_decoding/config/file_path.py
+++ src/movie_decoding/config/file_path.py
@@ -1,4 +1,8 @@
 from pathlib import Path
 
-PATIENTS_FILE_PATH = Path(__file__).resolve().parents[3] / "data/patients"
-SURROGATE_FILE_PATH = Path(__file__).resolve().parents[3] / "data/surrogate_windows"
+ROOT_PATH = Path(__file__).resolve().parents[3]
+DATA_PATH = ROOT_PATH / "data"
+PATIENTS_FILE_PATH = ROOT_PATH / "data/patients"
+SURROGATE_FILE_PATH = ROOT_PATH / "data/surrogate_windows"
+CONFIG_FILE_PATH = ROOT_PATH / "config"
+RESULT_PATH = ROOT_PATH / "results"
